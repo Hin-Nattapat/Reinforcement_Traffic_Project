501ef65270f17b9f90f3c8ccc7e9cc767d004fca
TW2541 <31178813+TW2541@users.noreply.github.com>
2020-09-18 18:13:56 +0700

    Revert "Add Function control state TrafficLight"
    This reverts commit 07ae09f43a91f7a22d882c9509bd753cc6f5b48d.

--- 4cross_TLS/Traci_Test.py
+++ 4cross_TLS/Traci_Test.py
@@ -16,7 +16,6 @@ from sumolib import checkBinary
 import traci
 import traci.constants as tc
 
-
 def get_options():
     optParser = optparse.OptionParser()
     optParser.add_option("--nogui", action="store_true",
@@ -24,94 +23,75 @@ def get_options():
     options, args = optParser.parse_args()
     return options
 
-
 def Get_Vehicel_Fuction():
-    while traci.simulation.getMinExpectedNumber() > 0:
+    while traci.simulation.getMinExpectedNumber() > 0: 
         # แสดงรถทั้งหมดในระบบ ณ เวลานั้นๆ
         vehicle_id_list = traci.vehicle.getIDList()
         # แสดงเวลารอของรถแต่ละคัน (ทั้งหมด ณ ขณะนั้น)
-        print([traci.vehicle.getWaitingTime(vehicle_id)
-               for vehicle_id in vehicle_id_list])
+        print([traci.vehicle.getWaitingTime(vehicle_id) for vehicle_id in vehicle_id_list]) 
         # แสดงเวลารอสะสมของรถแต่ละคัน (ทั้งหมด ณ ขณะนั้น)
-        # print([traci.vehicle.getAccumulatedWaitingTime(vehicle_id) for vehicle_id in vehicle_id_list])
+        # print([traci.vehicle.getAccumulatedWaitingTime(vehicle_id) for vehicle_id in vehicle_id_list]) 
         traci.simulationStep()
     traci.close()
     sys.stdout.flush()
-
 
 def Get_Lane_Fuction():
-    while traci.simulation.getMinExpectedNumber() > 0:
+    while traci.simulation.getMinExpectedNumber() > 0: 
         # แสดงเลนทั้งหมด
-        # print(traci.lane.getIDList())
+        # print(traci.lane.getIDList()) 
         # จำนวนรถที่หยุด
-        # print(traci.lane.getLastStepHaltingNumber('gneE10_0'))
+        # print(traci.lane.getLastStepHaltingNumber('gneE10_0')) 
         # เวลารอของรถทุกคันในเลนนั้นๆ
-        # print(traci.lane.getWaitingTime('gneE10_0'))
+        # print(traci.lane.getWaitingTime('gneE10_0')) 
         traci.simulationStep()
     traci.close()
     sys.stdout.flush()
-
 
 def Get_TLS_Fuction():
-    while traci.simulation.getMinExpectedNumber() > 0:
-        if (traci.simulation.getCurrentTime()) == 150000 :
-            Set_TLS_Fuction()
+    while traci.simulation.getMinExpectedNumber() > 0: 
         # แสดงไฟจราจรทั้งหมด
-        # print(traci.trafficlight.getIDList())
+        # print(traci.trafficlight.getIDList()) 
         # แสดง State TLS ขณะนั้น
-        # print(traci.trafficlight.getRedYellowGreenState('gneJ7'))
+        # print(traci.trafficlight.getRedYellowGreenState('gneJ8'))
         # เวลาที่จะเปลี่ยนเป็น Next State
-        # print(traci.trafficlight.getNextSwitch('gneJ7'))
-        # เวลาของ State นั้่นๆ
-        print(traci.trafficlight.getPhaseDuration('gneJ7'))
+        # print(traci.trafficlight.getNextSwitch('gneJ8')) 
+        # เวลาของ State นั้่นๆ 
+        # print(traci.trafficlight.getPhaseDuration('gneJ8')) 
         # ข้อมูลของ State ทั้งหมด
-        # print(traci.trafficlight.getCompleteRedYellowGreenDefinition('gneJ7'))
-        # Index ของ State ไฟจราจรใน file .net.xml
-        # print(traci.trafficlight.getPhase('gneJ7'))
+        # print(traci.trafficlight.getCompleteRedYellowGreenDefinition('gneJ8')) 
+        # Index ของ State ไฟจราจรใน file .net.xml 	
+        # print(traci.trafficlight.getPhase('gneJ8')) 
         # id ของชุด TLS
-        # print(traci.trafficlight.getProgram('gneJ7'))
+        # print(traci.trafficlight.getProgram('gneJ8'))
         traci.simulationStep()
     traci.close()
     sys.stdout.flush()
-
 
 def Get_Simulation_Fuction():
-    while traci.simulation.getMinExpectedNumber() > 0:
+    while traci.simulation.getMinExpectedNumber() > 0: 
         # หมายเลขรถที่กำลังเข้ามาใน Network
-        # print(traci.simulation.getDepartedIDList())
+        # print(traci.simulation.getDepartedIDList()) 
         # จำนวนรถที่อยู่ใน Network และที่รอเข้ามาใน Network
-        # print(traci.simulation.getMinExpectedNumber())
+        # print(traci.simulation.getMinExpectedNumber()) 
         traci.simulationStep()
     traci.close()
     sys.stdout.flush()
 
-
 def Set_TLS_Fuction():
-    phases = []
-    phases.append(traci.trafficlight.Phase(10, "rrrrrrrrrrrrGGGG", 0, 0, [], "setViaComplete"))
-    phases.append(traci.trafficlight.Phase(3, "rrrrrrrrrrrryyyy", 0, 0))
-    phases.append(traci.trafficlight.Phase(10, "rrrrGGGGrrrrrrrr", 0, 0))
-    phases.append(traci.trafficlight.Phase(3, "rrrryyyyrrrrrrrr", 0, 0))
-    phases.append(traci.trafficlight.Phase(10, "GGGGrrrrrrrrrrrr", 0, 0))
-    phases.append(traci.trafficlight.Phase(3, "yyyyrrrrrrrrrrrr", 0, 0))
-    phases.append(traci.trafficlight.Phase(10,  "rrrrrrrrGGGGrrrr", 0, 0))
-    phases.append(traci.trafficlight.Phase(3, "rrrrrrrryyyyrrrr", 0, 0))
-    logic = traci.trafficlight.Logic("state_1", 0, 0, phases)
-    traci.trafficlight.setProgramLogic('gneJ7', logic)
     
-    # while traci.simulation.getMinExpectedNumber() > 0:
-    #     traci.simulationStep()
-    # set ค่าเวลาของ Index ณ ขณะนั้น(เปลี่ยนแปลงแค่ช่วงเวลานั้นๆ)
-    # traci.trafficlight.setPhaseDuration('gneJ8',20.00)
-    # set State ของ Index ณ ขณะนั้น(เปลี่ยนแปลงแค่ช่วงเวลานั้นๆ)
-    # traci.trafficlight.setRedYellowGreenState('gneJ8','yyyrrryyyrrr')
-
-    # หากต้องการตั้งค่าของการเปลี่ยน TLS จะใช้ [คำสั่งเดียวกัน]
-    # traci.trafficlight.setProgramLogic('gneJ8',)
-    # traci.trafficlight.setCompleteRedYellowGreenDefinition('gneJ8',)
-    # traci.close()
-    # sys.stdout.flush()
-
+    while traci.simulation.getMinExpectedNumber() > 0:
+        traci.simulationStep() 
+        # set ค่าเวลาของ Index ณ ขณะนั้น(เปลี่ยนแปลงแค่ช่วงเวลานั้นๆ)
+        # traci.trafficlight.setPhaseDuration('gneJ8',20.00)
+        # set State ของ Index ณ ขณะนั้น(เปลี่ยนแปลงแค่ช่วงเวลานั้นๆ)
+        # traci.trafficlight.setRedYellowGreenState('gneJ8','yyyrrryyyrrr')
+        
+        # หากต้องการตั้งค่าของการเปลี่ยน TLS จะใช้ [คำสั่งเดียวกัน]
+        # traci.trafficlight.setProgramLogic('gneJ8',)
+        # traci.trafficlight.setCompleteRedYellowGreenDefinition('gneJ8',)
+    traci.close()
+    sys.stdout.flush()
+   
 
 if __name__ == "__main__":
     options = get_options()
@@ -120,9 +100,8 @@ if __name__ == "__main__":
     else:
         sumoBinary = checkBinary('sumo-gui')
     traci.start([sumoBinary, "-c", "4cross_TLS/1_1Cross.sumocfg"])
-
-
-# Get_Vehicel_Fuction()
-# Get_Lane_Fuction()
-    Get_TLS_Fuction()
-# Get_Simulation_Fuction()
+    # Get_Vehicel_Fuction()
+    # Get_Lane_Fuction()
+    # Get_TLS_Fuction()
+    # Get_Simulation_Fuction()
+    # Set_TLS_Fuction()

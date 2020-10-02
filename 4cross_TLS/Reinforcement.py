ee051c0b94ba8719e5b41c95221bb3a7a2b47925
Nattapat_p <60010325@kmitl.ac.th>
2020-10-02 21:26:07 +0700

    initStateSpace fuction

--- 4cross_TLS/Reinforcement.py
+++ 4cross_TLS/Reinforcement.py
@@ -23,6 +23,7 @@ num_episodes = 5
 # State จะอยู่ในรูปแบบของ [G1,G2,G3] เวลาไฟเขียวแต่ละแยก
 # 1 Cycle = 120 วินาที ดังนั้น G4 = 120-G1-G2-G3
 
+
 class SumoEnv:
     def __init__(self, net_file, rou_file, use_gui):
         self.net_file = net_file
@@ -38,9 +39,11 @@ class SumoEnv:
 
 
 class TrafficLight:
-    def __init__(self, phases, state, action, reward):
-        self.phases = phases
-        self.state = [15, 15, 15]
+    def __init__(self, state):
+        # self.phases = phases
+        # self.reward = reward
+        self.state = state
+        self.stateSpace = []
         self.action = [
             [self.state[0]+15, self.state[1], self.state[2]],
             [self.state[0]-15, self.state[1], self.state[2]],
@@ -49,30 +52,40 @@ class TrafficLight:
             [self.state[0], self.state[1], self.state[2]+15],
             [self.state[0], self.state[1], self.state[2]-15],
         ]
-        self.reward = reward
+        print(self.state)
 
     def setTLS(self):
         TrafficLightPhases = []
         G4 = 120-self.state[0]-self.state[1]-self.state[2]
-        TrafficLightPhases.append(traci.trafficlight.Phase(self.state[0], "rrrrrrrrrrrrGGGG", 0, 0, [], "setViaComplete"))
-        TrafficLightPhases.append(traci.trafficlight.Phase(3, "rrrrrrrrrrrryyyy", 0, 0))
-        TrafficLightPhases.append(traci.trafficlight.Phase(self.state[1], "rrrrGGGGrrrrrrrr", 0, 0))
-        TrafficLightPhases.append(traci.trafficlight.Phase(3, "rrrryyyyrrrrrrrr", 0, 0))
-        TrafficLightPhases.append(traci.trafficlight.Phase(self.state[2], "GGGGrrrrrrrrrrrr", 0, 0))
-        TrafficLightPhases.append(traci.trafficlight.Phase(3, "yyyyrrrrrrrrrrrr", 0, 0))
-        TrafficLightPhases.append(traci.trafficlight.Phase(G4,  "rrrrrrrrGGGGrrrr", 0, 0))
-        TrafficLightPhases.append(traci.trafficlight.Phase(3, "rrrrrrrryyyyrrrr", 0, 0))
+        TrafficLightPhases.append(traci.trafficlight.Phase(
+            self.state[0], "rrrrrrrrrrrrGGGG", 0, 0, [], "setViaComplete"))
+        TrafficLightPhases.append(
+            traci.trafficlight.Phase(3, "rrrrrrrrrrrryyyy", 0, 0))
+        TrafficLightPhases.append(traci.trafficlight.Phase(
+            self.state[1], "rrrrGGGGrrrrrrrr", 0, 0))
+        TrafficLightPhases.append(
+            traci.trafficlight.Phase(3, "rrrryyyyrrrrrrrr", 0, 0))
+        TrafficLightPhases.append(traci.trafficlight.Phase(
+            self.state[2], "GGGGrrrrrrrrrrrr", 0, 0))
+        TrafficLightPhases.append(
+            traci.trafficlight.Phase(3, "yyyyrrrrrrrrrrrr", 0, 0))
+        TrafficLightPhases.append(
+            traci.trafficlight.Phase(G4,  "rrrrrrrrGGGGrrrr", 0, 0))
+        TrafficLightPhases.append(
+            traci.trafficlight.Phase(3, "rrrrrrrryyyyrrrr", 0, 0))
         logic = traci.trafficlight.Logic("InitState", 0, 0, TrafficLightPhases)
         traci.trafficlight.setProgramLogic('gneJ7', logic)
 
     def get_waiting_time(self):
         wait_time = [0.0, 0.0, 0.0, 0.0]
         keep = True
-        lane = [['gneE3_0', 'gneE3_1'], ['gneE13_0', 'gneE13_1'], ['gneE11_0', 'gneE11_1'], ['gneE7_0', 'gneE7_1']]
+        lane = [['gneE3_0', 'gneE3_1'], ['gneE13_0', 'gneE13_1'],
+                ['gneE11_0', 'gneE11_1'], ['gneE7_0', 'gneE7_1']]
         while traci.simulation.getMinExpectedNumber() > 0:
             phase = traci.trafficlight.getPhase('gneJ7')
             if phase % 2 == 0 and keep:
-                temp = (traci.lane.getWaitingTime(lane[int(phase/2)][0]) + traci.lane.getWaitingTime(lane[int(phase/2)][1])) / 2
+                temp = (traci.lane.getWaitingTime(
+                    lane[int(phase/2)][0]) + traci.lane.getWaitingTime(lane[int(phase/2)][1])) / 2
                 wait_time[int(phase/2)] = temp
                 print(temp)
                 keep = False
@@ -85,3 +98,28 @@ class TrafficLight:
             traci.simulationStep()
         traci.close()
         sys.stdout.flush()
+
+    def randomAction(self):
+        action = randrange(0, 6)
+        self.state = self.action[action]
+        return self.state
+
+    def InitStateSpace(self):
+        self.stateSpace.append([self.state[0],self.state[1],self.state[2],0])
+        while self.state != [75, 75, 75]:
+            self.state[2] += 15
+            if self.state[2] == 90:
+                self.state[2] = 15
+                self.state[1] += 15
+                if self.state[1] == 90:
+                    self.state[1] = 15
+                    self.state[0] += 15
+            self.stateSpace.append([self.state[0],self.state[1],self.state[2],0])
+        return self.stateSpace
+
+
+State = [15, 15, 15]
+x = TrafficLight(State)
+# State = x.randomAction()
+test = x.InitStateSpace()
+print(test[0][3])

2141dfdc9826d651e6181a8903b2d40214c747af
Nattapat_p <60010325@kmitl.ac.th>
2020-11-05 11:46:24 +0700

    Add Vehicle Random Change Density, and Loop True

--- api.py
+++ api.py
@@ -1,6 +1,6 @@
 import traci
 import pandas
-
+import random
 
 def get_waiting_time(lane, NextState):
     wait_time = [0.0, 0.0, 0.0, 0.0]
@@ -19,12 +19,7 @@ def get_waiting_time(lane, NextState):
             count += 1
         elif phase % 2 == 0:
             keep = True
-        #   print(traci.trafficlight.getPhaseDuration('gneJ7'))
-        # print(traci.trafficlight.getNextSwitch('gneJ7'))
-        # print(traci.simulation.getTime())
-        # if (traci.simulation.getTime() % 132 == 0 and traci.simulation.getTime() != 0):
-        #     print("TESTTTTTTTT")
-        #     set_Trafficlight(NextState)
+        random_Vehicle()
         traci.simulationStep()
     return sum(wait_time) / len(wait_time)
 
@@ -71,8 +66,62 @@ def csv_data_stateSpace(data):
     print(dataframe)
 
 
+def add_Route():
+    traci.route.add("rou_1", ["gneE43", "gneE3", "gneE6", "gneE40"])
+    traci.route.add("rou_2", ["gneE43", "gneE3", "gneE12", "gneE32"])
+    traci.route.add("rou_3", ["gneE43", "gneE3", "gneE10", "gneE34"])
+    traci.route.add("rou_4", ["gneE41", "gneE7", "gneE8", "gneE42"])
+    traci.route.add("rou_5", ["gneE41", "gneE7", "gneE12", "gneE32"])
+    traci.route.add("rou_6", ["gneE41", "gneE7", "gneE10", "gneE34"])
+    traci.route.add("rou_7", ["gneE35", "gneE11", "gneE8", "gneE42"])
+    traci.route.add("rou_8", ["gneE35", "gneE11", "gneE6", "gneE40"])
+    traci.route.add("rou_9", ["gneE35", "gneE11", "gneE12", "gneE32"])
+    traci.route.add("rou_10", ["gneE33", "gneE13", "gneE8", "gneE42"])
+    traci.route.add("rou_11", ["gneE33", "gneE13", "gneE6", "gneE40"])
+    traci.route.add("rou_12", ["gneE33", "gneE13", "gneE10", "gneE34"])
+    # traci.route.add("rou_10", ["gneE43", "gneE3", "gneE6", "gneE7", "gneE12", "gneE32"])
+    # traci.route.add("rou_11", ["gneE43", "gneE3", "gneE6", "gneE7", "gneE10", "gneE34"])
+    # traci.route.add("rou_12", ["gneE43", "gneE3", "gneE6", "gneE7", "gneE8", "gneE42"])
+    # traci.route.add("rou_13", ["gneE43", "gneE3", "gneE12", "gneE13", "gneE6", "gneE40"])
+    # traci.route.add("rou_14", ["gneE43", "gneE3", "gneE12", "gneE13", "gneE10", "gneE34"])
+    # traci.route.add("rou_15", ["gneE43", "gneE3", "gneE12", "gneE13", "gneE8", "gneE42"])
+    # traci.route.add("rou_16", ["gneE43", "gneE3", "gneE10", "gneE11", "gneE12", "gneE32"])
+    # traci.route.add("rou_17", ["gneE43", "gneE3", "gneE10", "gneE11", "gneE6", "gneE40"])
+    # traci.route.add("rou_18", ["gneE43", "gneE3", "gneE10", "gneE11", "gneE8", "gneE42"])
+    # traci.route.add("rou_19", ["gneE41", "gneE7", "gneE8", "gneE3", "gneE12", "gneE32"])
+    # traci.route.add("rou_20", ["gneE41", "gneE7", "gneE8", "gneE3", "gneE10", "gneE34"])
+    # traci.route.add("rou_21", ["gneE41", "gneE7", "gneE8", "gneE3", "gneE6", "gneE40"])
+    # traci.route.add("rou_22", ["gneE41", "gneE7", "gneE12", "gneE13", "gneE6", "gneE40"])
+    # traci.route.add("rou_23", ["gneE41", "gneE7", "gneE12", "gneE13", "gneE10", "gneE34"])
+    # traci.route.add("rou_24", ["gneE41", "gneE7", "gneE12", "gneE13", "gneE8", "gneE42"])
+    # traci.route.add("rou_25", ["gneE41", "gneE7", "gneE10", "gneE11", "gneE12", "gneE32"])
+    # traci.route.add("rou_26", ["gneE41", "gneE7", "gneE10", "gneE11", "gneE6", "gneE40"])
+    # traci.route.add("rou_27", ["gneE41", "gneE7", "gneE10", "gneE11", "gneE8", "gneE42"])
+    # traci.route.add("rou_28", ["gneE33", "gneE13", "gneE8", "gneE3", "gneE12", "gneE32"])
+    # traci.route.add("rou_29", ["gneE33", "gneE13", "gneE8", "gneE3", "gneE10", "gneE34"])
+    # traci.route.add("rou_30", ["gneE33", "gneE13", "gneE8", "gneE3", "gneE6", "gneE40"])
+    # traci.route.add("rou_31", ["gneE33", "gneE13", "gneE6", "gneE7", "gneE12", "gneE32"])
+    # traci.route.add("rou_32", ["gneE33", "gneE13", "gneE6", "gneE7", "gneE10", "gneE34"])
+    # traci.route.add("rou_33", ["gneE33", "gneE13", "gneE6", "gneE7", "gneE8", "gneE42"])
+    # traci.route.add("rou_34", ["gneE33", "gneE13", "gneE10", "gneE11", "gneE12", "gneE32"])
+    # traci.route.add("rou_35", ["gneE33", "gneE13", "gneE10", "gneE11", "gneE6", "gneE40"])
+    # traci.route.add("rou_36", ["gneE33", "gneE13", "gneE10", "gneE11", "gneE8", "gneE42"])
+    # traci.route.add("rou_37", ["gneE35", "gneE11", "gneE8", "gneE3", "gneE12", "gneE32"])
+    # traci.route.add("rou_38", ["gneE35", "gneE11", "gneE8", "gneE3", "gneE10", "gneE34"])
+    # traci.route.add("rou_39", ["gneE35", "gneE11", "gneE8", "gneE3", "gneE6", "gneE40"])
+    # traci.route.add("rou_40", ["gneE35", "gneE11", "gneE6", "gneE7", "gneE12", "gneE32"])
+    # traci.route.add("rou_41", ["gneE35", "gneE11", "gneE6", "gneE7", "gneE10", "gneE34"])
+    # traci.route.add("rou_42", ["gneE35", "gneE11", "gneE6", "gneE7", "gneE8", "gneE42"])
+    # traci.route.add("rou_43", ["gneE35", "gneE11", "gneE12", "gneE13", "gneE10", "gneE34"])
+    # traci.route.add("rou_44", ["gneE35", "gneE11", "gneE12", "gneE13", "gneE6", "gneE40"])
+    # traci.route.add("rou_45", ["gneE35", "gneE11", "gneE12", "gneE13", "gneE8", "gneE42"])
+
+
 def random_Vehicle():
-    traci.route.add("rou_1",["gneE43", "gneE3", "gneE12","gneE32"])
-    
-    for i in range(100):
-        traci.vehicle.add(str(i+1),"rou_1")
+    i = traci.simulation.getTime()
+    if i % 2 == 0 :
+        RouteID = traci.route.getIDList()
+        count = traci.route.getIDCount()
+        index = random.randint(0, count-1)
+        traci.vehicle.add("vehicle_"+str(i),RouteID[index], departSpeed="desired",)
+        traci.vehicle.setMaxSpeed("vehicle_"+str(i), 25.0)
--- runner.py
+++ runner.py
@@ -28,10 +28,11 @@ if __name__ == "__main__":
             ['gneE11_0', 'gneE11_1'], ['gneE7_0', 'gneE7_1']]
     initState = [15, 15, 15]
     MAX_EPOCHS = 1000
+    EPOCHS = 0
     CYCLE = 132
     TIME = MAX_EPOCHS*CYCLE
     rl = RL.TrafficLight(initState, lane)
-    
+
     options = get_options()
     if options.nogui:
         sumoBinary = checkBinary('sumo')
@@ -40,36 +41,17 @@ if __name__ == "__main__":
 
     traci.start([sumoBinary, "-c", "4cross_TLS/1_1Cross.sumocfg"])
     rl.InitStateSpace()
-    
-    traci.route.add("rou_1",["gneE43", "gneE3", "gneE12","gneE32"])
-    traci.route.add("rou_2",["gneE33", "gneE13","gneE10","gneE34"])
-    traci.route.add("rou_3",["gneE35", "gneE11","gneE6","gneE40"])
-    traci.route.add("rou_4",["gneE41", "gneE7", "gneE10", "gneE11", "gneE6", "gneE40"])
-    traci.route.add("rou_5",["gneE43", "gneE3", "gneE12", "gneE13", "gneE8", "gneE42"])
-    traci.route.add("rou_6",["gneE33" ,"gneE13", "gneE8", "gneE42"])
-    traci.route.add("rou_7",["gneE33" ,"gneE13", "gneE8", "gneE3", "gneE12", "gneE32"])
-    traci.route.add("rou_8",["gneE35", "gneE11", "gneE6", "gneE7","gneE10", "gneE34"])
-    traci.route.add("rou_9",["gneE41","gneE7","gneE12","gneE32"])
-    traci.route.add("rou_10",["gneE41", "gneE7", "gneE10","gneE34"])
-    traci.route.add("rou_11",["gneE33","gneE13","gneE6","gneE40"])
-    traci.route.add("rou_12",["gneE43", "gneE3", "gneE10", "gneE34"])
-    
-    RouteID = traci.route.getIDList()
-    print("Test :",RouteID[1])
-    for i in range(10000):
-        index = random.randint(0,11)
-        traci.vehicle.add("vehicle_"+str(i),RouteID[index],departSpeed="desired",)
-        traci.vehicle.setMaxSpeed("vehicle_"+str(i),25.0)
-        traci.simulationStep()
 
+    api.add_Route()
     # os.chdir("./4cross_TLS")
     # os.system('python randomTrips.py --net-file=1_1Cross.net.xml --route-file=1_1Cross.rou.xml --weights-prefix=1_1Cross --end='+str(TIME)+' --fringe-factor=10 --period=2.5 --trip-attributes="departLane=\'best\' departSpeed=\'max\' departPos=\'random\'"  -l --validate --fringe-factor 10  --max-distance 2000')
 
     # for i in range(MAX_EPOCHS):
-    #     print("----------------------------- EPOCHS: ",i, "-----------------------------")
-    #     # api.random_vehicle(i)
-    #     rl.P_Greedy_Al()
-    #     rl.updateFuction()
-    #     rl.updateState()
-    #     print("----------------------------------------------------------------------")
+    while True:
+        print("----------------------------- EPOCHS: ",EPOCHS, "-----------------------------")
+        rl.P_Greedy_Al()
+        rl.updateFuction()
+        rl.updateState()
+        EPOCHS = EPOCHS+1
+        print("----------------------------------------------------------------------")
 sys.stdout.flush()
